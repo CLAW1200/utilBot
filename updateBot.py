501661927a2122840b95931d14b4ff789fa15a89
CLAW1200 <carstenwood12@gmail.com>
2023-11-04 20:02:19 +0000

    updated updater

---
+++ updateBot.py
@@ -0,0 +1,57 @@
+import os
+import utilityBeltLib as ublib
+
+def update_bot():
+    """
+    Update the bot and restart it
+    """        
+    #end the bot 
+    ublib.exit_bot()
+
+    # Check if the bot is up to date
+    if not is_bot_version_latest():
+        os.system("git pull")
+
+    #Start the bot
+    os.system("python3 app.py")
+    return
+
+def is_bot_version_latest():
+    """
+    Check for an update to the bot via github
+    """
+    # Get the current commit hash
+    current_commit_hash = get_current_commit_hash()
+
+    # Get the latest commit hash from github
+    latest_commit_hash = get_latest_commit_hash()
+
+    # Check if the bot is up to date
+    if current_commit_hash == latest_commit_hash:
+        return True
+    else:
+        return False
+    
+def get_current_commit_hash():
+    """
+    Get the current commit hash of the bot
+    """
+    # Get the current commit hash
+    with open(".git/refs/heads/master", "r") as f:
+        current_commit_hash = f.read().strip()
+    print (f"Current commit hash: {current_commit_hash}")
+    return current_commit_hash
+
+def get_latest_commit_hash():
+    """
+    Use git command line to get the latest commit hash from github
+    """
+    # get latest commit hash from github
+    os.system("git fetch") # fetch the latest commit hash
+    os.system("git reset --hard origin/master") # reset the local repo to the latest commit hash
+    with open(".git/refs/heads/master", "r") as f:
+        latest_commit_hash = f.read().strip().strip("'")
+    print (f"Latest commit hash: {latest_commit_hash}")
+
+if __name__ == "__main__":
+    update_bot()
--- utilityBeltLib.py
+++ utilityBeltLib.py
@@ -11,61 +11,11 @@ import json
 import datetime
 import urllib
 import shutil
-import app
+import sys
 
-def restart_script():
-    app.main()
-
-def update_bot():
-    """
-    Update the bot and restart it
-    """        
-    # Pull the latest commit
-    os.system("git pull")
-
-    # Restart the bot
-    restart_script()
-
-
-def is_bot_version_latest():
-    """
-    Check for an update to the bot via github
-    """
-    # Get the current commit hash
-    current_commit_hash = get_current_commit_hash()
-
-    # Get the latest commit hash from github
-    latest_commit_hash = get_latest_commit_hash()
-
-    # Check if the bot is up to date
-    if current_commit_hash == latest_commit_hash:
-        return True
-    else:
-        return False
-    
-def get_current_commit_hash():
-    """
-    Get the current commit hash of the bot
-    """
-    # Get the current commit hash
-    with open(".git/refs/heads/master", "r") as f:
-        current_commit_hash = f.read().strip()
-    print (f"Current commit hash: {current_commit_hash}")
-    return current_commit_hash
-
-def get_latest_commit_hash():
-    """
-    Use git command line to get the latest commit hash from github
-    """
-    # get latest commit hash from github
-    os.system("git fetch")
-    os.system("git reset --hard origin/master")
-    with open(".git/refs/heads/master", "r") as f:
-        latest_commit_hash = f.read().strip().strip("'")
-    print (f"Latest commit hash: {latest_commit_hash}")
-
-
-
+def exit_bot():
+    log.info("Exiting bot")
+    sys.exit()
 
 MESSAGE = 25
 DIRECT_MESSAGE = 30
