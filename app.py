df43a09a612f99f576a92ca792820c798114ef1b
CLAW1200 <carstenwood12@gmail.com>
2023-12-27 21:47:33 +0000

    fixed maybe !stats command

--- app.py
+++ app.py
@@ -1132,7 +1132,7 @@ def main():
                     draw_guilds = True
                 if "command" in message.content:
                     draw_commands = True
-                if "command" and "user" and "guild" not in message.content:
+                if "command" or "user" or "guild" not in message.content:
                     draw_users = True
                     draw_guilds = True
                     draw_commands = True
