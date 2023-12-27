a9b1a1d46b9cc5dcd8e34987d626c80c83b6c4af
CLAW1200 <carstenwood12@gmail.com>
2023-12-27 22:11:54 +0000

    okay should be all good now

--- app.py
+++ app.py
@@ -1132,11 +1132,10 @@ def main():
                     draw_guilds = True
                 if "command" in message.content:
                     draw_commands = True
-                if "all" in message.content:
+                if "user" not in message.content and "guild" not in message.content and "command" not in message.content:
                     draw_users = True
                     draw_guilds = True
                     draw_commands = True
-
                 if "-t " in message.content:
                     # get everything after -t 
                     time = message.content.split("-t ")[1]
