1d3c8f4f2c71e6a9485645f8bc9398fa6cbb85fc
CLAW1200 <carstenwood12@gmail.com>
2024-01-01 14:19:56 +0000

    test

--- app.py
+++ app.py
@@ -1240,7 +1240,7 @@ def main():
             if message.content.startswith("!help"):
                 # https://cdn.discordapp.com/emojis/1191381954453586061.gif?size=96&quality=lossless
                 # https://discord.com/channels/1170496731872493739/1178817154620067851/1191382117972717649
-                custom_emoji = '<:loading:1191382117972717649>'
+                loading_emoji = '<a:loading:1191381954453586061> '
                 await botOwner.send(f"""**!help** - Send this message
 **!guilds** - Send a list of guilds the bot is in
 **!log** - Send the log file
@@ -1260,7 +1260,7 @@ def main():
 **!guildlookup** - Search a guild ID
 **!invme** - Create a guild invite
 **!stats** - Send the data.csv file
-{custom_emoji}
+{loading_emoji}
                 """)
 
     bot.response_messages = {}
