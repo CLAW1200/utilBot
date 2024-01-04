ca9a485dc0e4bf22bb604ac90c8fc2469a5689a3
CLAW1200 <carstenwood12@gmail.com>
2024-01-04 17:55:18 +0000

    testing

--- sdtturbo.py
+++ sdtturbo.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 try:
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.set_headless()
-    brower = webdriver.Firefox(firefox_options=fireFoxOptions)
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    brower = webdriver.Firefox(firefox_options=options)
 
     brower.get('https://pythonbasics.org')
     print(brower.page_source)
