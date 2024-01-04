c8591945ec40bd1cb16809ee90c2db7c3783d393
CLAW1200 <carstenwood12@gmail.com>
2024-01-04 18:27:00 +0000

    test

--- sdtturbo.py
+++ sdtturbo.py
@@ -4,5 +4,4 @@ options = Options()
 options.binary_location = r'/usr/bin/firefox-esr'
 from selenium.webdriver.firefox.service import Service
 service = Service('/home/pi/.local/bin/geckodriver')
-driver = webdriver.Firefox()
-print ("1")
+driver = webdriver.Firefox(options=options, service=service)
