5a714bfe13c5b25fcc64a9ccb6e96365bfd00f14
claw1200 <carstenwood12@gmail.com>
2024-05-13 16:50:12 +0000

    Removed unused files

--- test.py
+++
@@ -1,9 +0,0 @@
-import utilityBeltLib as ub
-ub.add_speech_bubble("https://media.discordapp.net/attachments/1172190810930937930/1235230979787067452/upgrade15png.png?ex=66339dff&is=66324c7f&hm=4533390aa353e4446e84016c63443cc12ead3fc3b091b999b4f078f1fa8db136&=&format=webp&quality=lossless&width=660&height=660", 0.2)
-
-
-# https://cdn.discordapp.com/attachments/1098539870823133244/1235244178632343624/FuGQp3_WYBgGxmq.jpg?ex=6633aa49&is=663258c9&hm=372d32f3100318bc87f1323d394a7116254792321559c2aa55df25746ec14a6d&
-
-# https://media.discordapp.net/attachments/1098539870823133244/1235244265853157488/FuGQp3_WYBgGxmq.jpg?ex=6633aa5e&is=663258de&hm=6d4d2fa3aa852d1ec457bcfec4948c432c60af17c5772bd12d55a32b36d78ef1&=&format=webp&width=481&height=722
-
-# https://cdn.discordapp.com/attachments/1098539870823133244/1235244178632343624/FuGQp3_WYBgGxmq.jpg?ex=6633aa
